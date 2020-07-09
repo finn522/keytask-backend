ed4b43dc17c4acccc06b03ea7bbc738f93c1bbf7
Simon de Bakker <s.de.bakker@live.nl>
2020-07-09 16:36:48 +0200

    updated query

--- pipeline.js
+++ pipeline.js
@@ -4,9 +4,12 @@ module.exports = {
     postPushResult: app.post("/push-result", function (req, res) {
         console.log(req.body.test)
         connection.query(
-          "INSERT INTO pipeline_inserts (test) VALUES (?)",
+          "INSERT INTO pipeline_inserts (commit_message, test_results, total_tests, tests_passed) VALUES (?, ?, ?, ?)",
           [
-            req.body.test
+            req.body.commit_message,
+            req.body.test_results,
+            req.body.total_tests,
+            req.body.tests_passed
           ],
           function (error, results, fields) {
             if (error) {
